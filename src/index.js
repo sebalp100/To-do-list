f91afc7a76ff9b909755b4c77ce617818402da68
sebalp100 <sebalp100@gmail.com>
2022-11-01 22:48:14 -0300

    Add showTask class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import './style.css';
 
 /* eslint-disable  no-unused-vars */
 import restart from './icons/restart.svg';
+
 import enter from './icons/enter.svg';
+
 import dots from './icons/dots.svg';
+
 import Actions from '../modules/actions.js';
 
-document.addEventListener('DOMContentLoaded', Actions.addElem());
+const input = document.querySelector('.list-create input');
+
+let data = JSON.parse(localStorage.getItem('todo-list'));
+
+document.addEventListener('DOMContentLoaded', Actions.showTask(data));
+
+input.addEventListener('keyup', (e) => {
+  const task = input.value;
+  if (e.key === 'Enter' && task) {
+    if (!data) {
+      data = [];
+    }
+    input.value = '';
+    const taskInfo = { description: task, completed: false, index: data.length };
+    data.push(taskInfo);
+    localStorage.setItem('todo-list', JSON.stringify(data));
+    Actions.showTask(data);
+  }
+});
